f60f37072116157c5016182d0a1b42a0efe863b5
Ethan <eolivier48@gmail.com>
2018-02-15 10:40:35 -0600

    added eloquent links for visits to people and states.

--- app/People.php
+++ app/People.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class People extends Model
 {
     protected $fillable = ['first_name', 'last_name', 'favorite_food'];
+
+    public function visits()
+    {
+      return $this->hasMany(Visit::class,'person_id');
+    }
 }
--- app/State.php
+++ app/State.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class State extends Model
 {
     protected $fillable = ['state_name', 'state_abbreviation'];
+
+    public function visits()
+    {
+      return $this->hasMany(Visit::class);
+    }
 }
