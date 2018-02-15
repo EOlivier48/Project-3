03c744d20613953cc42ece9ab6ce527752eb999e
Ethan <eolivier48@gmail.com>
2018-02-15 16:30:19 -0600

    deleted unnesasary function boilerplates in all controllers

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -7,102 +7,58 @@ use Illuminate\Http\Request;
 
 class PeopleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+      $people = People::get();
+      return JSON_encode(compact('people'));
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $data = $request->json()->all();
+    $rules = [
+      'first_name' => 'required',
+      'last_name' => 'required',
+      'favorite_food' => 'required'
+    ];
+
+    $validator = \Validator::make($data,$rules);
+
+    if($validator->passes())
     {
-        $people = People::get();
-        return JSON_encode(compact('people'));
+      People::create([
+        'first_name' => $data['first_name'],
+        'last_name' => $data['last_name'],
+        'favorite_food' => $data['favorite_food']
+      ]);
+      return response("Success",200);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    else
     {
-        //
+        return response("Bad Request",400);
     }
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      $data = $request->json()->all();
-      $rules = [
-        'first_name' => 'required',
-        'last_name' => 'required',
-        'favorite_food' => 'required'
-      ];
-
-      $validator = \Validator::make($data,$rules);
-
-      if($validator->passes())
-      {
-        People::create([
-          'first_name' => $data['first_name'],
-          'last_name' => $data['last_name'],
-          'favorite_food' => $data['favorite_food']
-        ]);
-        return response("Success",200);
-      }
-      else
-      {
-          return response("Bad Request",400);
-      }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $people = People::find($id);
-        return JSON_encode(compact('people'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+      $people = People::find($id);
+      return JSON_encode(compact('people'));
+  }
 }
--- app/Http/Controllers/VisitsController.php
+++ app/Http/Controllers/VisitsController.php
@@ -7,114 +7,70 @@ use App\Visit;
 
 class VisitsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+      //GET visits
+      $visits = Visit::get();
+      return JSON_encode(compact('visits'));
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $data = $request->json()->all();
+    //refactored the giant if statement to use laravel's validator.
+    //while not necessarily smaller it is definitely more readable.
+    //if ((array_key_exists('person_id',$data) && isset($data['person_id'])) && (array_key_exists('state_id',$data) && isset($data['state_id'])) && (array_key_exists('date_visited',$data) && isset($data['date_visited'])))
+    $rules = [
+      'person_id' => 'required',
+      'state_id' => 'required',
+      'date_visited' => 'required'
+    ];
+
+    $validator = \Validator::make($data,$rules);
+
+    if($validator->passes())
     {
-        //GET visits
-        $visits = Visit::get();
-        return JSON_encode(compact('visits'));
+      Visit::create([
+        'person_id' => $data['person_id'],
+        'state_id' => $data['state_id'],
+        'date_visited' => $data['date_visited']
+      ]);
+      return response("Success",200);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    else
     {
-        //
+        return response("Bad Request",400);
     }
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      $data = $request->json()->all();
-      //refactored the giant if statement to use laravel's validator.
-      //while not necessarily smaller it is definitely more readable.
-      //if ((array_key_exists('person_id',$data) && isset($data['person_id'])) && (array_key_exists('state_id',$data) && isset($data['state_id'])) && (array_key_exists('date_visited',$data) && isset($data['date_visited'])))
-      $rules = [
-        'person_id' => 'required',
-        'state_id' => 'required',
-        'date_visited' => 'required'
-      ];
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($visit)
+  {
+      //GET vists/{id}
+      return JSON_encode(compact('visit'));
+  }
 
-      $validator = \Validator::make($data,$rules);
-
-      if($validator->passes())
-      {
-        Visit::create([
-          'person_id' => $data['person_id'],
-          'state_id' => $data['state_id'],
-          'date_visited' => $data['date_visited']
-        ]);
-        return response("Success",200);
-      }
-      else
-      {
-          return response("Bad Request",400);
-      }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($visit)
-    {
-        //GET vists/{id}
-        return JSON_encode(compact('visit'));
-    }
-
-    public function showVisitsForPerson($id)
-    {
-        //show all visits for person
-        //$visits = Visit::where('person_id', $id)->get();
-        $visits = \App\People::find($id)->visits;
-        return JSON_encode(compact('visits'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  public function showVisitsForPerson($id)
+  {
+      //show all visits for person
+      //$visits = Visit::where('person_id', $id)->get();
+      $visits = \App\People::find($id)->visits;
+      return JSON_encode(compact('visits'));
+  }
 }
